814fd5dbc8d83c10e69dcefc1ea2721852425e01
unknown <ruby@D407-04.fundacion-jala.local>
2015-09-02 09:40:09 -0400

    Feature file for the lab16

---
+++ Alex/lab16/features/support/helper/helper.rb
@@ -0,0 +1,35 @@
+#Add the library for HTTP
+require 'net/http'
+
+module Rest_service
+
+  HTTP_TIMEOUT_FOR_RESPONSE = 180
+  
+  #
+  # This method set up a connection 
+  #
+  def Rest_service.get_connection
+      http_connection = nil
+      http_connection = Net::HTTP.new($app_host)      
+      http_connection.read_timeout = HTTP_TIMEOUT_FOR_RESPONSE
+      return http_connection
+  end
+
+  def Rest_service.get_request(method, url)
+    request = nil   
+    url =  $app_root + url
+   
+    case method
+      when "POST"
+        request = Net::HTTP::Post.new(url)
+      when "PUT"
+        request = Net::HTTP::Put.new(url)
+      when "DELETE"
+        request = Net::HTTP::Delete.new(url)
+      when "GET"
+        request = Net::HTTP::Get.new(url)
+    end
+   return request
+  end
+
+end
